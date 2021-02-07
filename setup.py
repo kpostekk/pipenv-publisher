736ddfb4001733231cf28505b8751097fbabd81d
Krystian Postek <krystian@postek.eu>
2021-02-07 20:24:36 +0100

    Mark as alpha 2

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ from setuptools import setup
 
 auto_detect = {}
 
+# RST detection
+if Path.cwd().joinpath('README.rst').exists():
+    with Path.cwd().joinpath('README.rst').open('r') as f:
+        auto_detect = auto_detect | {'long_description': f.read()}
+
+# Markdown detection
 if Path.cwd().joinpath('README.md').exists():
     with Path.cwd().joinpath('README.md').open('r') as f:
         auto_detect = auto_detect | {'long_description_content_type': "text/markdown", 'long_description': f.read()}
